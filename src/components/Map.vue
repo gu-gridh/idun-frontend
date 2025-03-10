8bfcc1f5b8af3755455bf1c031d849f429c7a0d0
kikkipedia <kristin.akerlund@lir.gu.se>
2025-03-10 15:09:07 +0100

    Added leaflet + map component

---
+++ src/components/Map.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+onMounted(() => {
+    let map = L.map('map').setView([0, 0], 1);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+});
+</script>
+
+<style scoped>
+#map {
+    height: inherit;
+    width: inherit;
+    border-radius: 15px;
+    z-index: auto;
+}
+</style>
