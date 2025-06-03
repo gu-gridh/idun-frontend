d85c91c6b558e8e7d619e0e91b20a49d62bac352
kikkipedia <kristin.akerlund@lir.gu.se>
2025-06-03 11:13:34 +0200

    css width for first page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -775,7 +775,7 @@
     }
 
     .intro-space {
-      width: calc(100% - 60px);
+      width: calc(100% - 100px);
     }
 
     .text-module-link {
@@ -793,7 +793,7 @@
     .info-module-slogan h2 {
       color: var(--theme-heading);
       margin-top: -30px !important;
-      /* max-width: calc(100% - 80px); */
+      max-width: calc(100% - 50px);
       font-weight: 300;
       font-size: 20px;
     }
