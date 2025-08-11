f66f8e90a7aaff3a85e1a12f2ccd7eee6dc41bbc
Julia Beck <j4lib@users.noreply.github.com>
2025-08-11 11:10:42 +0200

    Update links to forskarlagring

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -185,7 +185,7 @@
           <h3>Highlighted Resources</h3>
           <div class="projects">
             <a href="https://saintsophia.dh.gu.se" class="project-item"
-              style="border-radius:10px 0 0 10px; background-image:url(https://idun.dh.gu.se/files/large/bc1bb1c389814bae436d6328357d21a0946b3358.jpg); ">
+              style="border-radius:10px 0 0 10px; background-image:url(https://data.dh.gu.se/idun/files/large/bc1bb1c389814bae436d6328357d21a0946b3358.jpg); ">
               <!-- <div class="project-image-round" style="background-image:url(https://idun.dh.gu.se/files/large/bc1bb1c389814bae436d6328357d21a0946b3358.jpg); background-size: cover; background-position: 50%;"></div> -->
               <div class="project-title">Saint Sophia's Inscriptions</div>
           </a>
@@ -193,17 +193,17 @@
 
 
             <a href="https://didoc.dh.gu.se" class="project-item"
-              style="background-image:url(https://idun.dh.gu.se/files/large/fb5565201c7c7bb2e9423cfdc88f42405b98ec0e.jpg);">
+              style="background-image:url(https://data.dh.gu.se/idun/files/large/fb5565201c7c7bb2e9423cfdc88f42405b98ec0e.jpg);">
 
               <!-- <div class="project-image-round" style="background-image:url(https://idun.dh.gu.se/files/large/fb5565201c7c7bb2e9423cfdc88f42405b98ec0e.jpg); background-size: cover; background-position: 50%;"></div> -->
               <div class="project-title">Dawit Isaak Database of Censorship</div>
         </a>
 
             <a href="https://etruscan.dh.gu.se" class="project-item"
-              style="border-radius:0px 10px 10px 0px;  background-image:url(https://idun.dh.gu.se/files/large/f64a8a5f8606fd201d3a6706172468169f1b150f.jpg);">
+              style="border-radius:0px 10px 10px 0px;  background-image:url(https://data.dh.gu.se/idun/files/large/f64a8a5f8606fd201d3a6706172468169f1b150f.jpg);">
        
                
-                  <!-- <div class="project-image-round" style="background-image:url(https://idun.dh.gu.se/files/large/f64a8a5f8606fd201d3a6706172468169f1b150f.jpg); background-size: cover; background-position: 50%;"></div> -->
+                  <!-- <div class="project-image-round" style="background-image:url(https://data.dh.gu.se/idun/files/large/f64a8a5f8606fd201d3a6706172468169f1b150f.jpg); background-size: cover; background-position: 50%;"></div> -->
                   <div class="project-title">Etruscan Chamber Tombs</div>
             
         
