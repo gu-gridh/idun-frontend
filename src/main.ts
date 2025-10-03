25b535c975369a9a67480fef97d5628086f495cf
Tristan Bridge <tbridge360@gmail.com>
2025-10-03 08:53:19 +0200

    matomo tracking

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import VueMatomo from 'vue-matomo'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueMatomo, {
+    host: import.meta.env.VITE_MATOMO_URL,
+    siteId: import.meta.env.VITE_MATOMO_SITE_ID,
+    router,
+    enableLinkTracking: true,
+    trackInitialView: true,
+    debug: false
+})
 
 app.mount('#app')
---
+++ src/vue-matomo.d.ts
@@ -0,0 +1,5 @@
+declare module 'vue-matomo' {
+  import type { Plugin } from 'vue'
+  const plugin: Plugin
+  export default plugin
+}
